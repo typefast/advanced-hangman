0df5bae60404974f2ed5c94a217982f9b87fe8b6
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-05 10:37:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Creating a more advanced hangman game.
---
+++ hangman.rb
@@ -0,0 +1,6 @@
+#load in the dictionary and randomly select a word between 5 and 12 characters long (secret_word)
+#Show guessed letters in the word
+#show list of incorrect letters
+#show guesses left. 
+#each turn allow the guess of a letter guess_letter
+#update display to reflect correct on incorrect guess if out of gueeses the player should lose
