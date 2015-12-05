467aed74f88fa8471053d2c9d06d29f2c9dbc013
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-05 11:00:39 +0000

    Add random_word method

--- hangman.rb
+++ hangman.rb
@@ -4,3 +4,22 @@
 #show guesses left. 
 #each turn allow the guess of a letter guess_letter
 #update display to reflect correct on incorrect guess if out of gueeses the player should lose
+class Hangman
+  
+  def random_word
+    dictionary = File.open('desk.txt', 'r')
+    words = []
+    
+    dictionary.each_line do |word|
+        words << word.chomp.downcase if word.length <= 12 && word.length >= 5
+    end
+    words.sample
+  end
+  
+  def guess_letter
+  end
+end
+
+hangman = Hangman.new
+secret_word = hangman.random_word
+puts secret_word
